30ffe3a87fd88bb93d085b335e71ebd8141be4a7
Mustard <mustard.mar@gmail.com>
2023-02-17 21:06:39 +0300

    Добавьте файлы проекта.

---
+++ SimpleCSharpApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimpleCSharpApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** My First C# App *******");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+        }
+    }
+}
