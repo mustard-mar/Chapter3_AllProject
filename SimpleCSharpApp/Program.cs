db84d2546f377f70d95d8d1343869dfda1bd302a
Mustard <mustard.mar@gmail.com>
2023-02-17 21:17:02 +0300

    Использование операторов верхнего уровня

--- SimpleCSharpApp/Program.cs
+++ SimpleCSharpApp/Program.cs
@@ -1,15 +1,4 @@
 ﻿using System;
-
-namespace SimpleCSharpApp // Note: actual namespace depends on the project name.
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("***** My First C# App *******");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
-
-        }
-    }
-}
+Console.WriteLine("***** My First C# App *******");
+Console.WriteLine("Hello World!");
+Console.WriteLine();
